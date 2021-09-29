ba7ac0e1c5f0ca30b74e019949364ebe428c307e
brady-mcm <brady.mcmechan@gmail.com>
2021-09-29 11:35:20 -0700

    Update main.py

--- appengine/standard_python3/hello_world/main.py
+++ appengine/standard_python3/hello_world/main.py
@@ -30,9 +30,7 @@ def hello():
 def versA():
     return 'This is app version B!'
 
-@app.route('/about')
-def about():
-    return 'The about page.'
+
 if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
     # Engine, a webserver process such as Gunicorn will serve the app. This
